3d1eb3e5003c34086cdba3752e2aa7b0a5247fa4
Ryan McLaughlin <rpmclaughlin18@gmail.com>
2021-11-03 16:58:00 -0700

    fix

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -53,7 +53,6 @@ export class Router {
           history.pushState({page: page}, "", " ");
         else
           history.pushState({page: page}, "", (location.hash)+"#"+page);
-        history.forward();
       }
     }
  }
